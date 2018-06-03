0b47926e25305aa8ec4e0f41aefaa84e0ac76532
Carles Sallés <csallesbaro@gmail.com>
2018-06-03 13:01:46 +0200

    Pregunta top 10 casi acabada

--- codi/top10.R
+++ codi/top10.R
@@ -1,7 +1,9 @@
+# install.packages('ggplot2')
+# library(ggplot2)
+
 exploitsData <- read.csv(file="C:/Users/Carles/Documents/exploitDataSet.csv", header=TRUE, sep=",")
 
 authors <- exploitsData$author
-
 
 authorsTable <- table(authors)
 authorsDistinct <- unique(authors)
@@ -13,5 +15,25 @@ for (author in authorsDistinct) {
   topTen <- c(topTen, authorsTable[names(authorsTable)==author])
 }
 
+orderedTopTen <- sort(topTen, decreasing = TRUE)
 
+pieVector <- head(orderedTopTen, n = 10)
+
+top10labels <- names(pieVector)
+
+piepercent <- round(100*pieVector/sum(pieVector), 1)
+
+pie(pieVector, labels = piepercent, main = "Top 10 exploit developers",col = rainbow(length(pieVector)))
+legend("topright", top10labels, cex = 0.8,
+      fill = rainbow(length(pieVector)))
+# 
+# df <- data.frame(
+#   group = top10labels,
+#   value = pieVector
+# )
+# 
+# bp<- ggplot(df, aes(x="", y=value, fill=group))+
+#   geom_bar(width = 1, stat = "identity")
+# 
+# pie <- bp + coord_polar("y")
 
